0efbc7788ffe19a7ad45628ef24d999613ae9eeb
DyloreAquino <lutherjerold@gmail.com>
2025-11-19 00:04:36 +0800

    Some fixes because errors

--- Player/Weapon Manager/Projectiles/rigid_body_projectile.gd
+++ Player/Weapon Manager/Projectiles/rigid_body_projectile.gd
@@ -12,7 +12,8 @@ func _set_weapon_projectile(weapon: Pistol) -> void:
 func launch_rigid_body_projectile(point: Vector3, weapon: Pistol, bullet: PackedScene) -> void:
 	var _projectile: RigidBody3D = bullet.instantiate()
 	
-	_projectile.top_level = false
+	get_tree().current_scene.add_child(_projectile)
+	
 	_projectile.global_position = weapon.bullet_point.global_position
 	
 	_projectile.look_at(point)
@@ -20,4 +21,4 @@ func launch_rigid_body_projectile(point: Vector3, weapon: Pistol, bullet: Packed
 	var direction: Vector3 = (point - weapon.bullet_point.global_position).normalized()
 	_projectile.set_linear_velocity(direction * projectile_velocity)
 	
-	get_tree().current_scene.add_child(_projectile)
+	
