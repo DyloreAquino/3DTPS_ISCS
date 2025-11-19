99033b763be7157e7dee9b4b82cb95d2be23da3b
DyloreAquino <lutherjerold@gmail.com>
2025-11-20 03:52:04 +0800

    Remove dependency on player rotation

--- Player/playeranims.gd
+++ Player/playeranims.gd
@@ -4,6 +4,9 @@ extends Node3D
 
 var current_aim_state : float = 0.0
 
+func _ready():
+	top_level = true
+
 func set_walk_direction(dir : Vector2):
 	a_tree.set("parameters/WalkDirection/blend_position", dir)
 
@@ -17,9 +20,14 @@ func activate_shoot():
 func get_root_pos():
 	return a_tree.get_root_motion_position()
 
+func _process(delta):
+	global_position = get_parent().global_position
+	global_rotation.y = get_parent().global_rotation.y + PI
+
 func _physics_process(delta):
 	a_tree.set("parameters/TorsoState/blend_position", current_aim_state)
 	a_tree.set("parameters/AimState/blend_position", current_aim_state)
+	
 
 func _on_sprint_sprint_end():
 	a_tree.set("parameters/TimeScale/scale", 1.5)
