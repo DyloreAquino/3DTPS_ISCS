43a8bab4ac24bcef9a8153b1962bcb2d1449f8cb
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2025-11-19 21:23:16 +0800

    ex
    plosiom

--- Player/Weapon Manager/Projectiles/pistol_bullet.gd
+++ Player/Weapon Manager/Projectiles/pistol_bullet.gd
@@ -1,7 +1,14 @@
 extends RigidBody3D
 
+var ex = preload("res://World/explosion.tscn")
+
 func _on_timer_timeout() -> void:
 	queue_free()
 
 func _on_body_entered(body: Node) -> void:
+	var plosion = ex.instantiate()
+	plosion.global_position = self.global_position
+	get_tree().current_scene.add_child(plosion)
 	queue_free()
+	
+	
