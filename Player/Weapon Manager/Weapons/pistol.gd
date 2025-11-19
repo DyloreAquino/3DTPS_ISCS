decf2e43da184a3762c296852af6f1dc420386b4
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2025-11-19 23:01:21 +0800

    s h o o t

--- Player/Weapon Manager/Weapons/pistol.gd
+++ Player/Weapon Manager/Weapons/pistol.gd
@@ -4,11 +4,24 @@ class_name Pistol
 @export var bullet_point: Marker3D
 @export var current_ammo: Ammo
 
+var shootTimer: float = 1.0
+var isShooting: bool = false
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("shoot"):
-		print("Shoot")
-		$RigidBodyProjectile._set_weapon_projectile(self)
-	
 	if event.is_action_pressed("swap_cam"):
 		print("Swap")
 		$".".position.x = -position.x
+
+func _process(delta):
+	if Input.is_action_pressed("shoot"):
+		if isShooting:
+			shootTimer += delta
+			if shootTimer > 1:
+				$RigidBodyProjectile._set_weapon_projectile(self)
+				shootTimer = 0.0
+		else:
+			$RigidBodyProjectile._set_weapon_projectile(self)
+			isShooting = true
+			shootTimer = 0.0
+	if Input.is_action_just_released("shoot"):
+		isShooting = false
