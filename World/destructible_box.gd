0acd5e45637ef9a02b2f1af3a242edcd58710e04
DyloreAquino <lutherjerold@gmail.com>
2025-11-20 09:35:05 +0800

    Last

--- World/destructible_box.gd
+++ World/destructible_box.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 func die():
-	$RigidBody3D/Area3D/CollisionShape3D.disabled = true
 	$AudioStreamPlayer3D.play()
 	$RigidBody3D.queue_free()
 	await get_tree().create_timer(1.0).timeout
